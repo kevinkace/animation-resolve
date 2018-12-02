0f7886d155c3fe0ed7569556ce513f56b45d1906
kevinkace <kevinacameron@hotmail.com>
2018-12-02 14:16:49 -0800

    :poop: quality tests

---
+++ test/index.js
@@ -0,0 +1,77 @@
+// import animResolve from "./animation-resolve";
+const animResolve = require("../dist/cjs");
+
+m.mount(window.mount, {
+    toggleElements() {
+        this.show = !this.show;
+        this.showSvg = !this.showSvg;
+
+        m.redraw();
+    },
+
+    toggleAdd() {
+        this.add = !this.add;
+
+        m.redraw();
+    },
+
+    view(vnode) {
+        return m("div",
+            m("button", { onclick : () => vnode.state.toggleElements() }, "toggle elements"),
+            m("button", { onclick : () => vnode.state.toggleAdd() }, "toggle add"),
+
+            // reg dom
+            vnode.state.show ?
+                m("div", {
+                        class : "color",
+
+                        onbeforeremove({ dom }) {
+                            return animResolve(dom, "animOut", { add : vnode.state.add });
+                        }
+                    },
+                    "child"
+                ) :
+                null,
+
+            // dom timeout before anim complete
+            vnode.state.show ?
+                m("div", {
+                        class : "color",
+
+                        onbeforeremove({ dom }) {
+                            return animResolve(dom, "animOutShort", { add : vnode.state.add });
+                        }
+                    },
+                    "child"
+                ) :
+                null,
+
+            // anim much longer than timeout
+            vnode.state.show ?
+                m("div", {
+                        class : "color",
+
+                        onbeforeremove({ dom }) {
+                            return animResolve(dom, "animOut", { timeout : 500, add : vnode.state.add });
+                        }
+                    },
+                    "child"
+                ) :
+                null,
+
+            // svg
+            vnode.state.showSvg ?
+                m("svg", {
+                            class   : "color",
+                            viewBox : "0 0 32 32",
+
+                            onbeforeremove({ dom }) {
+                                return animResolve(dom, "animOut", { add : vnode.state.add });
+                            }
+                        },
+                        m("path", { d : "M27 4l-15 15-7-7-5 5 12 12 20-20z" })
+                ) :
+                null
+        );
+    }
+});
